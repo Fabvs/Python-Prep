47d27e95639aa6e43d51be4b7e7b02c3e523a63c
Fabvs <fabioabr2000@gmail.com>
2022-12-21 18:53:01 -0500

    first commit

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,11 @@
-print('Hola Mundo!222')
+#Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
+
+edad = int(input("Introduce tu edad: "))
+if edad < 18:
+  print("Eres menor de edad y no tributas")
+elif edad >= 18:
+  ingreso = int(input("Introduce tu ingreso mensual: "))
+  if ingreso >= 1000:
+    print("Tributas")
+  elif ingreso < 1000:
+    print("No tributas")
